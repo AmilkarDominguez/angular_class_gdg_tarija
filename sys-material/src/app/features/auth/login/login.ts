06ae731fdfbf3ed561af2b595e098eee64b734c4
AmilkarDominguez <amilkar.dominguez@gmail.com>
2026-03-18 21:51:34 -0400

    chore: login form

--- sys-material/src/app/features/auth/login/login.ts
+++ sys-material/src/app/features/auth/login/login.ts
@@ -1,21 +1,58 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+
+interface LoginData {
+  email: string;
+  password: string;
+}
+
+import {
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { email, form, FormField, required, submit } from '@angular/forms/signals';
+import { MatButton } from '@angular/material/button';
+import { ProductsService } from '../../../core/services/products-service';
 
 @Component({
   selector: 'app-login',
-  imports: [MatSelectModule,
+  imports: [
+    MatSelectModule,
     MatInputModule,
-    MatFormFieldModule, ReactiveFormsModule, FormsModule],
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    FormsModule,
+    FormField,
+    MatButton,
+  ],
   templateUrl: './login.html',
   styleUrl: './login.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class Login {
 
-  readonly email = new FormControl('', [Validators.required, Validators.email]);
+  loginModel = signal<LoginData>({
+    email: '',
+    password: '',
+  });
 
-  email2!: string;
+  loginForm = form(this.loginModel, (schemaPath) => {
+    required(schemaPath.email, { message: 'Correo es requerido' });
+    email(schemaPath.email, { message: 'Correo invalido' });
+    required(schemaPath.password, { message: 'Constrasena es requerido' });
+  });
+
+  onSubmit(event: Event) {
+
+    event.preventDefault();
+    submit(this.loginForm, {
+      action: async () => {
+        const credentials = this.loginModel();
+        console.log('Credenciales de login:', credentials);
+      },
+    });
+  }
 
 }
